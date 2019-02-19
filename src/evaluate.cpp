085653fbf6a89aeb41b1913c08c6ea4e44f218a9
Lolligerhans <billaddresse@gmx.de>
2019-02-19 20:21:04 +0100

    slim fishtest patch (removed new blocked heuristic)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -511,8 +511,10 @@ namespace {
     // square with a pawn, or because they defend the square twice and we don't.
     stronglyProtected =  attackedBy[Them][PAWN]
                        | (attackedBy2[Them] & ~attackedBy2[Us]);
-    constexpr Score OwnedBonus = make_score(6, 4);
-    score -= popcount(stronglyProtected | (attackedBy[Them][ALL_PIECES] & ~attackedBy[Us][ALL_PIECES]));
+
+    constexpr Score OwnedBonus = make_score(5, 5);
+    score -= OwnedBonus * popcount(stronglyProtected |
+                  (attackedBy[Them][ALL_PIECES] & ~attackedBy[Us][ALL_PIECES]));
 
     // Non-pawn enemies, strongly protected
     defended = nonPawnEnemies & stronglyProtected;
