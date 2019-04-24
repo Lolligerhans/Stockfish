0096982b49a9f41ebb51d4b1c9e0cea5b1f2d335
Lolligerhans <billadresse@gmx.de>
2019-04-25 00:16:32 +0200

    Translate old Blowmob repo to new SF patches
    Had to add some bitboards (forwardRanks and kingFlank) again.
    Might need some optimisation by removing calls in existing code.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -31,6 +31,14 @@ Bitboard SquareBB[SQUARE_NB];
 Bitboard LineBB[SQUARE_NB][SQUARE_NB];
 Bitboard PseudoAttacks[PIECE_TYPE_NB][SQUARE_NB];
 Bitboard PawnAttacks[COLOR_NB][SQUARE_NB];
+Bitboard ForwardRanksBB[COLOR_NB][SQUARE_NB];
+Bitboard SeverityMidblockBB[SQUARE_NB];
+
+Bitboard KingFlank[FILE_NB] = {
+  QueenSide ^ FileDBB, QueenSide, QueenSide,
+  CenterFiles, CenterFiles,
+  KingSide, KingSide, KingSide ^ FileEBB
+};
 
 Magic RookMagics[SQUARE_NB];
 Magic BishopMagics[SQUARE_NB];
@@ -74,9 +82,26 @@ void Bitboards::init() {
   for (Square s = SQ_A1; s <= SQ_H8; ++s)
       SquareBB[s] = (1ULL << s);
 
+  for (Rank r = RANK_1; r < RANK_8; ++r)
+      ForwardRanksBB[WHITE][r] = ~(ForwardRanksBB[BLACK][r + 1] = ForwardRanksBB[BLACK][r] | rank_bb(r));
+
   for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
       for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
               SquareDistance[s1][s2] = std::max(distance<File>(s1, s2), distance<Rank>(s1, s2));
+
+  // severity bitboards
+  for (Square s = SQ_A1; s <= SQ_H8; ++s)
+  {
+      // inwards and same file
+      for( File f = FILE_A; f <= FILE_H; ++f)
+      {
+          if (file_of(s) <= FILE_D && file_of(s) <= f && f <= FILE_E)
+              SeverityMidblockBB[s] |= file_bb(f);
+          else
+          if (file_of(s) >= FILE_E && file_of(s) >= f && f >= FILE_D)
+              SeverityMidblockBB[s] |= file_bb(f);
+      }
+  }
 
   int steps[][5] = { {}, { 7, 9 }, { 6, 10, 15, 17 }, {}, {}, {}, { 1, 7, 8, 9 } };
 
