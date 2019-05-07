8113f5e2766fb163a686688fdf01963fb5aab24f
Lolligerhans <billaddresse@gmx.de>
2019-05-07 21:04:19 +0200

    Add penalty for encapsulated minors
    Minors are considered encapsulated iff their mobility lies solely
    within the first 3 ranks.
    
    Bench: 3175596

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -135,6 +135,7 @@ namespace {
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CorneredBishop     = S( 50, 50);
+  constexpr Score EncapsulatedMinor  = S( 10, 50);
   constexpr Score FlankAttacks       = S(  8,  0);
   constexpr Score Hanging            = S( 69, 36);
   constexpr Score KingProtector      = S(  7,  8);
@@ -270,6 +271,8 @@ namespace {
     constexpr Direction Down = (Us == WHITE ? SOUTH : NORTH);
     constexpr Bitboard OutpostRanks = (Us == WHITE ? Rank4BB | Rank5BB | Rank6BB
                                                    : Rank5BB | Rank4BB | Rank3BB);
+    constexpr Bitboard HighRanks    = (Us == WHITE ? ~(Rank1BB | Rank2BB | Rank3BB)
+                                                   : ~(Rank6BB | Rank7BB | Rank8BB));
     const Square* pl = pos.squares<Pt>(Us);
 
     Bitboard b, bb;
@@ -298,7 +301,8 @@ namespace {
             kingAttacksCount[Us] += popcount(b & attackedBy[Them][KING]);
         }
 
-        int mob = popcount(b & mobilityArea[Us]);
+        Bitboard m = b & mobilityArea[Us];
+        int mob = popcount(m);
 
         mobility[Us] += MobilityBonus[Pt - 2][mob];
 
@@ -320,6 +324,9 @@ namespace {
 
             // Penalty if the piece is far from the king
             score -= KingProtector * distance(s, pos.square<KING>(Us));
+
+            // Penalty for encapsulated minor (has no mobility in higher ranks).
+            score -= EncapsulatedMinor * !bool(m & HighRanks);
 
             if (Pt == BISHOP)
             {
