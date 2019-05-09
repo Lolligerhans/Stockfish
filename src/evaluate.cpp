d8aaf709e9a1e26ac2aff251b3aa83f56beecb70
Lolligerhans <billadresse@gmx.de>
2019-05-09 13:36:15 +0200

    Change penalty 20,10
    Bench: 3135598

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -134,7 +134,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  constexpr Score BlowmobPenalty     = S( 10,  5);
+  constexpr Score BlowmobPenalty     = S( 20, 10);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score FlankAttacks       = S(  8,  0);
   constexpr Score Hanging            = S( 69, 36);
