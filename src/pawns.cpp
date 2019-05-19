8253617b78588857bda92a7003905c895de38cb5
Lolligerhans <billadresse@gmx.de>
2019-05-19 16:15:55 +0200

    Take 7 mp: real conservative
    Like master, but only add potential passer if no sacrifice needed.
    
    NOTE: Previous take (5) sais "conservative" but is different. Take 5
    gives all regular potential passers PLUS those which have the sac square
    doubly defended (=no sac needed no matter the opponent).
    
    Bench: 3389400

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -121,8 +121,9 @@ namespace {
             while (b)
             {
                 Square sacSquare = pop_lsb(&b);
-                if (  !more_than_one(theirPawns & PawnAttacks[  Us][sacSquare])
-                    || more_than_one(  ourPawns & PawnAttacks[Them][sacSquare]))
+                if (  (!more_than_one(theirPawns & PawnAttacks[  Us][sacSquare]) &&
+                                        ourPawns & PawnAttacks[Them][sacSquare])
+                    ||  more_than_one(  ourPawns & PawnAttacks[Them][sacSquare]))
                     e->passedPawns[Us] |= s;
             }
         }
