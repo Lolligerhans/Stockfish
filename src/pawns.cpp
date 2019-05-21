6104a7c3a5b189325a60ef206f5c4c9b0e0ca7a1
Lolligerhans <billadresse@gmx.de>
2019-05-21 12:00:48 +0200

    T5:mp_11: low sound-free
    Bench: 3484155

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -66,6 +66,7 @@ namespace {
 
     constexpr Color     Them = (Us == WHITE ? BLACK : WHITE);
     constexpr Direction Up   = (Us == WHITE ? NORTH : SOUTH);
+    constexpr Direction Down = -Up;
 
     Bitboard b, neighbours, stoppers, doubled, support, phalanx;
     Bitboard lever, leverPush;
@@ -86,8 +87,8 @@ namespace {
     {
         assert(pos.piece_on(s) == make_piece(Us, PAWN));
 
-        File f = file_of(s);
-        Rank r = relative_rank(Us, s);
+        File const f = file_of(s);
+        Rank const r = relative_rank(Us, s);
 
         e->pawnAttacksSpan[Us] |= pawn_attack_span(Us, s);
 
@@ -115,12 +116,23 @@ namespace {
             && popcount(phalanx) >= popcount(leverPush))
             e->passedPawns[Us] |= s;
 
-        else if (stoppers == square_bb(s + Up) && r >= RANK_5)
+        else if (stoppers == square_bb(s + Up))
         {
             b = shift<Up>(support) & ~theirPawns;
             while (b)
-                if (!more_than_one(theirPawns & PawnAttacks[Us][pop_lsb(&b)]))
-                    e->passedPawns[Us] |= s;
+            {
+                Square sacSquare = pop_lsb(&b);
+                if (r >= 5)
+                {
+                    if (!more_than_one(theirPawns & PawnAttacks[Us][sacSquare]))
+                        e->passedPawns[Us] |= s;
+                }
+                else
+                    if ((  !more_than_one(theirPawns & PawnAttacks[  Us][sacSquare]) &&
+                                         (  ourPawns & (s+Down)))
+                         || more_than_one(  ourPawns & PawnAttacks[Them][sacSquare]))
+                        e->passedPawns[Us] |= s;
+            }
         }
 
         // Score this pawn
