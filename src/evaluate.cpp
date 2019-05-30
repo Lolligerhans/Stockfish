26cb3407c2ea76053a003730dd58a47c16b0ad0d
Lolligerhans <billadresse@gmx.de>
2019-05-30 15:34:25 +0200

    blowmob_1: our bocked pawn block our pieces 20,20
    bench 3659373

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -134,8 +134,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  Score BlowmobPenalty     = S( 20, 20);
-  TUNE(BlowmobPenalty);
+  constexpr Score BlowmobPenalty     = S( 20, 20);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score FlankAttacks       = S(  8,  0);
   constexpr Score Hanging            = S( 69, 36);
