24c1cd47444cb9d939e66233ba427f0f902c7da2
Lolligerhans <billadresse@gmx.de>
2019-05-30 15:58:27 +0200

    Add tune macro
    bench 3774553

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -134,7 +134,8 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  constexpr Score BlowmobPenalty     = S( 10,  5);
+  Score BlowmobPenalty     = S( 10,  5);
+  TUNE(BlowmobPenalty);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score FlankAttacks       = S(  8,  0);
   constexpr Score Hanging            = S( 69, 36);
