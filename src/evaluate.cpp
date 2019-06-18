e00ecede5d8854dfbda67612fcc7cb08304f511a
Lolligerhans <billadresse@gmx.de>
2019-06-18 22:42:26 +0200

    Take 2: only xrays in sliding diraction
    bench 3387946x

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -279,7 +279,8 @@ namespace {
         // Find attacked squares, including x-ray attacks for bishops, rooks
         b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN))
           : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK))
-          : Pt ==  QUEEN ? attacks_bb(QUEEN, s, pos.pieces(Us, {PAWN, KNIGHT, KING}) | pos.pieces(Them))
+          : Pt ==  QUEEN ? attacks_bb<BISHOP>(s, pos.pieces() ^ (pos.pieces(Us, BISHOP) & PseudoAttacks[BISHOP][s]))
+                         | attacks_bb<  ROOK>(s, pos.pieces() ^ (pos.pieces(Us,   ROOK) & PseudoAttacks[  ROOK][s]))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.blockers_for_king(Us) & s)
