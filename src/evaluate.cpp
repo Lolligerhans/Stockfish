a557fc028800f8a1fcf97a29bcd2ffad2d04bf13
Lolligerhans <billadresse@gmx.de>
2019-06-18 22:47:15 +0200

    Take 5: factor 11 in pawns
    bench 3705780

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -739,7 +739,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    + 12 * pe->complexity()
+                    + 11 * pe->complexity()
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
