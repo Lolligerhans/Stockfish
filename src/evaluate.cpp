c6ec1b05251a43d4f0a625e5b2206949fc081a4c
Lolligerhans <lolligerhans@gmx.de>
2019-07-01 02:56:00 +0200

    shift down bishop outpost ranks
    bench 3891062

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -303,7 +303,8 @@ namespace {
         if (Pt == BISHOP || Pt == KNIGHT)
         {
             // Bonus if piece is on an outpost square or can reach one
-            bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
+            bb = (Pt == KNIGHT ? OutpostRanks : shift<Down>(OutpostRanks))
+                & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
             if (bb & s)
                 score += Outpost * (Pt == KNIGHT ? 2 : 1);
 
