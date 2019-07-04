1354ca553bbf6654732b2c4cafceee98b8584199
Lolligerhans <lolligerhans@gmx.de>
2019-07-05 01:24:51 +0200

    Only "winning" attacks for our passers secure them
    bench 3589416

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -642,7 +642,8 @@ namespace {
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
 
                 if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= attackedBy[Us][ALL_PIECES];
+                    defendedSquares &= (attackedBy[Us][ALL_PIECES] & ~attackedBy2[Them])
+                                    | attackedBy2[Us];
 
                 if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
