8e0e11cc9445e3bcc8f8bbfbb00b5eb3a3a03ab1
Lolligerhans <lolligerhans@gmx.de>
2019-07-05 07:14:48 +0200

    Reduced bonus for rook behind passer
    bench 3596270

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -641,8 +641,7 @@ namespace {
 
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
 
-                if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= attackedBy[Us][ALL_PIECES];
+                defendedSquares &= attackedBy[Us][ALL_PIECES] | (bool(pos.pieces(Us) & bb) ? square_bb(blockSq) : 0);
 
                 if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
