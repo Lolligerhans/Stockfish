739bcc7ab9fcad98bc923c9012f97edb471abad8
Lolligerhans <lolligerhans@gmx.de>
2019-07-05 07:22:50 +0200

    Yet another variant of _5
    bench 3734440

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -642,8 +642,7 @@ namespace {
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
 
                 if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= (attackedBy[Us][ALL_PIECES] & ~attackedBy2[Them])
-                                    | attackedBy2[Us];
+                    defendedSquares &= attackedBy[Us][ALL_PIECES] & ~attackedBy2[Them];
 
                 if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
