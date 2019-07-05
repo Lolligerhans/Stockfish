a24836acf304b46506a00722dce780c1de80bf3d
Lolligerhans <lolligerhans@gmx.de>
2019-07-05 07:42:54 +0200

    Space invaders
    bench 3966709

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -712,7 +712,8 @@ namespace {
     int weight = pos.count<ALL_PIECES>(Us) - 1;
     Score score = make_score(bonus * weight * weight / 16, 0);
 
-    score -= AttacksOnSpaceArea * popcount(attackedBy[Them][ALL_PIECES] & behind & safe);
+    score -= AttacksOnSpaceArea * popcount((attackedBy[Them][ALL_PIECES] & behind & safe)
+                                          | pos.pieces(Them));
 
     if (T)
         Trace::add(SPACE, Us, score);
