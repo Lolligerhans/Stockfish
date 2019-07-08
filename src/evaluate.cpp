aefcff9f6c9034f2cdf43de0c10c959d570387a8
31m059 <37052095+31m059@users.noreply.github.com>
2019-07-07 21:36:41 -0400

    Not defended if K-attacked & not K-defended. Bench: 3845980

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -642,7 +642,7 @@ namespace {
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
 
                 if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= attackedBy[Us][ALL_PIECES];
+                    defendedSquares &= attackedBy[Us][ALL_PIECES] & ~(attackedBy[Them][KING] & ~attackedBy[Us][KING]);
 
                 if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
