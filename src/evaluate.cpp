4427152a515eb34890840caf5f952a43b4a2d89e
Lolligerhans <lolligerhans@gmx.de>
2019-07-10 05:53:06 +0200

    Two defenders > 1 attacker for passers
    bench 3429028

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -641,8 +641,12 @@ namespace {
 
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
 
-                if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= attackedBy[Us][ALL_PIECES];
+                if (pos.pieces(Us) & bb)
+                    defendedSquares &= attackedBy[Us][ALL_PIECES]
+                                     | ~attackedBy2[Them];
+                else
+                    defendedSquares &= attackedBy2[Us]
+                                     | (attackedBy[Us][ALL_PIECES] & ~attackedBy2[Them]);
 
                 if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
