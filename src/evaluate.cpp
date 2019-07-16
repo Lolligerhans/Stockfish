a402f7e3b4b445090c7f63e59bd00a748f2961da
Lolligerhans <lolligerhans@gmx.de>
2019-07-16 15:46:41 +0200

    Attacks in squares neighboring unsafe squares only
    bench 3184983

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -647,7 +647,7 @@ namespace {
                 // If there are no enemy attacks on passed pawn span, assign a big bonus.
                 // Otherwise assign a smaller bonus if the path to queen is not attacked
                 // and even smaller bonus if it is attacked but block square is not.
-                int k = !unsafeSquares                    ? 35 :
+                int k = !(unsafeSquares & adjacent_files_bb(s))? 35 :
                         !(unsafeSquares & squaresToQueen) ? 20 :
                         !(unsafeSquares & blockSq)        ?  9 :
                                                              0 ;
