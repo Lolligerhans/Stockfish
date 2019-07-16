df310de9adc776edadbafb249a1cbc538d41af85
Lolligerhans <lolligerhans@gmx.de>
2019-07-16 17:07:18 +0200

    Relaxed mobility are (blocked pawns)
    If a pawn is removed from mobility are because it is blocked, still
    include it if it prevents a pawn of the opponent from advancing.
    
    bench 3305191

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -229,7 +229,9 @@ namespace {
     Bitboard dblAttackByPawn = pawn_double_attacks_bb<Us>(pos.pieces(Us, PAWN));
 
     // Find our pawns that are blocked or on the first two ranks
-    Bitboard b = pos.pieces(Us, PAWN) & (shift<Down>(pos.pieces()) | LowRanks);
+    Bitboard b = pos.pieces(Us, PAWN) & ( ( shift<Down>(pos.pieces())
+                                          & ~pe->pawn_attacks_span(Them))
+                                        | LowRanks);
 
     // Squares occupied by those pawns, by our king or queen or controlled by
     // enemy pawns are excluded from the mobility area.
