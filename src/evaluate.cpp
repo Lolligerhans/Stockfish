23065103e5f13385cf8b060d47dc7c52cbc715c9
Lolligerhans <lolligerhans@gmx.de>
2019-09-26 10:22:37 +0200

    Take 3: safe-check minor def w/o general bonus
    Retry tak 1 w/ compensation for the increased viability of N/B defenders
    by removing the generally applied bonuses for these pieces.
    
    bench 3710070

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -461,8 +461,8 @@ namespace {
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
-                 - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
-                 -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
+//                 - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
+//                 -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
                  - 873 * !pos.count<QUEEN>(Them)
