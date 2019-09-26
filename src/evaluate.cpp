7e115141b088aa4e098bfd3d356d8e0d610aa9d0
Lolligerhans <lolligerhans@gmx.de>
2019-09-26 10:08:24 +0200

    25 kingAttackersWeight for pawns
    bench 3824625

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -248,7 +248,8 @@ namespace {
         kingRing[Us] |= shift<EAST>(kingRing[Us]);
 
     kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
-    kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
+    kingAttacksCount[Them] = 0;
+    kingAttackersWeight[Them] = kingAttackersCount[Them] * 25;
 
     // Remove from kingRing[] the squares defended by two pawns
     kingRing[Us] &= ~dblAttackByPawn;
