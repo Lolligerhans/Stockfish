e40df5c8ea72864a461496c5f59a4912f4ddbda2
Lolligerhans <lolligerhans@gmx.de>
2019-12-01 05:15:03 +0100

    limit complexity to mg+cg and eg+og

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -722,8 +722,8 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
-    int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
+    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg+cg_value(score)));
+    int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg + eg_value(score)));
 
     if (T)
         Trace::add(INITIATIVE, make_score(u, v));
