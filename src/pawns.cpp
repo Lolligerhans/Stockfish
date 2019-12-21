6446e7bffaeb64881a5da685765478881e64edff
Lolligerhans <lolligerhans@gmx.de>
2019-12-21 14:38:32 +0100

    target front of pawns in endgames
    bench 5144312

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -221,6 +221,8 @@ Score Entry::evaluate_shelter(const Position& pos, Square ksq) {
 template<Color Us>
 Score Entry::do_king_safety(const Position& pos) {
 
+  auto constexpr Up = (Us == WHITE ? NORTH : SOUTH);
+
   Square ksq = pos.square<KING>(Us);
   kingSquares[Us] = ksq;
   castlingRights[Us] = pos.castling_rights(Us);
@@ -237,7 +239,7 @@ Score Entry::do_king_safety(const Position& pos) {
       shelter = std::max(shelter, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)), compare);
 
   // In endgame we like to bring our king near our closest pawn
-  Bitboard pawns = pos.pieces(Us, PAWN);
+  Bitboard pawns = shift<Up>(pos.pieces(Us, PAWN));
   int minPawnDist = pawns ? 8 : 0;
 
   if (pawns & PseudoAttacks[KING][ksq])
