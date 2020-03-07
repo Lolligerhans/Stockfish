f9299b4599a7defc8e9cb66e7cc4e125f04aa290
Lolligerhans <lolligerhans@gmx.de>
2020-03-07 15:57:58 +0100

    Reverse effect
    bench 5282615

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -295,7 +295,7 @@ namespace {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
             if (bb & s)
-                score += Outpost*2 * (Pt == KNIGHT ? 2 : 1) / (trapped ? 3 : 2);
+                score += Outpost*3 * (Pt == KNIGHT ? 2 : 1) / (trapped ? 2 : 3);
             else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
                 score += Outpost;
 
