c176957114382316f53a9c3740dba6dcd02d392a
Lolligerhans <lolligerhans@gmx.de>
2020-03-13 23:50:49 +0100

    Minor around pawn 5,5
    bench 5196829

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -301,10 +301,8 @@ namespace {
             // Knight and Bishop bonus for being right behind a pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
                 score += MinorBehindPawn;
-            if ((shift<EAST>(pos.pieces(PAWN))
-                |shift<WEST>(pos.pieces(PAWN))
-                ) & s);
-                score += MinorBehindPawn;
+            if (pos.attacks_from<KING>(s) & pos.pieces(PAWN))
+                score += make_score(5,5);
 
             // Penalty if the piece is far from the king
             score -= KingProtector * distance(s, pos.square<KING>(Us));
