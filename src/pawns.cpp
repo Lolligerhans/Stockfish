4fd67d52e1fbeef9b874fe133f577cf99027083d
Lolligerhans <lolligerhans@gmx.de>
2020-04-09 02:21:37 +0200

    Use logical or

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,7 +132,7 @@ namespace {
             e->passedPawns[Us] |= s;
 
         // Score this pawn
-        if (support | phalanx)
+        if (support || phalanx)
         {
             int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
                    + 21 * popcount(support);
