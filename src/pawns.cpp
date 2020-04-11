bf1528435165ba6dd81f96175882cfd07a8187a2
Lolligerhans <lolligerhans@gmx.de>
2020-04-11 17:23:12 +0200

    More connected bonus if passed
    bench 4918894

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -134,7 +134,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
+            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed) + bool(passed))
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
