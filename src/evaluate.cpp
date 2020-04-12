cc398a7fdef6daa002fe5a5f3b32a8abbf24b3da
Lolligerhans <lolligerhans@gmx.de>
2020-04-12 22:27:24 +0200

    Use pawns count only for space
    bench 4962953

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -678,7 +678,7 @@ namespace {
     behind |= shift<Down+Down>(behind);
 
     int bonus = popcount(safe) + popcount(behind & safe & ~attackedBy[Them][ALL_PIECES]);
-    int weight = pos.count<ALL_PIECES>(Us) - 1;
+    int weight = 2*pos.count<ALL_PIECES>(Us);
     Score score = make_score(bonus * weight * weight / 16, 0);
 
     if (T)
