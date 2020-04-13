9fe1828a656880a5201887f47b67adbfc459c353
Lolligerhans <lolligerhans@gmx.de>
2020-04-13 02:06:55 +0200

    Initiative penalty if useless bishop only
    bench 4699845

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -695,11 +695,31 @@ namespace {
   template<Tracing T>
   Score Evaluation<T>::initiative(Score score) const {
 
+    Value mg = mg_value(score);
+    Value eg = eg_value(score);
+
+    Color us = eg >= 0 ? WHITE : BLACK;
+
     int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                      - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
+
+    int pawnSnacks = 0;
+
+    // In bishop-only ending, if we can not attack any pawn with our bishop,
+    // -30 complexity. 0 complexity if we can attack 3. Rest linear.
+    if (pos.non_pawn_material(us) == BishopValueMg)
+    {
+        Bitboard atkPawns = pos.pieces(~us, PAWN);
+        if (DarkSquares & pos.pieces(us, BISHOP))
+            atkPawns &=  DarkSquares;
+        else
+            atkPawns &= ~DarkSquares;
+
+        pawnSnacks = 3 - popcount(atkPawns);
+    }
 
     bool almostUnwinnable =   outflanking < 0
                            && !pawnsOnBothFlanks;
@@ -715,10 +735,8 @@ namespace {
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
+                    + 10 * pawnSnacks
                     -110 ;
-
-    Value mg = mg_value(score);
-    Value eg = eg_value(score);
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
