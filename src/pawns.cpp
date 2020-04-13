48a5a2c1164030e67dc085a3e8b1ba5cb168528a
Lolligerhans <lolligerhans@gmx.de>
2020-04-13 09:21:31 +0200

    21 for phalanx
    bench 4817649

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -135,7 +135,7 @@ namespace {
         if (support | phalanx)
         {
             int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
-                   + 21 * popcount(support);
+                   + 21 * popcount(support|phalanx);
 
             score += make_score(v, v * (r - 2) / 4);
         }
