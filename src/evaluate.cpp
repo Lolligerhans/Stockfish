2bb11a2499b309ac8cf5cedf8a5ede6b4d37a8c2
Lolligerhans <lolligerhans@gmx.de>
2020-04-13 08:02:43 +0200

    No pawn within pawn span is open file
    bench 4989870

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -342,7 +342,7 @@ namespace {
 
             // Bonus for rook on an open or semi-open file
             if (pos.is_on_semiopen_file(Us, s))
-                score += RookOnFile[!(pos.pieces(Them, PAWN) & attackedBy[Them][PAWN] & file_bb(s))];
+                score += RookOnFile[!(pos.pieces(Them, PAWN) & pe->pawn_attacks_span(Them) & file_bb(s))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)
