c6543b5a07ba8d3b2f89e39e3da4e30bda163bdd
Lolligerhans <lolligerhans@gmx.de>
2020-04-16 22:10:21 +0200

    Take 2: more conservative monus
    bench 4682753

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -141,7 +141,7 @@ namespace {
             int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked)) / 2
                    + 21 * popcount(support);
 
-            v = v * 8 / (edge_distance(file_of(s))+5);
+            score += make_score(6, 3) * edge_distance(file_of(s));
 
             score += make_score(v, v * (r - 2) / 4);
         }
