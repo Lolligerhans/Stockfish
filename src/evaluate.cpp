08eba3d57c8683824f6cc0c621fb4ef03bd2bb66
Lolligerhans <lolligerhans@gmx.de>
2020-04-24 18:53:38 +0200

    Bonus for their pawns on same color squares as B
    bench 4786825

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -314,6 +314,7 @@ namespace {
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
                                      * (!(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles));
+                score += BishopPawns * pos.pawns_on_same_color_squares(Them, s);
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
