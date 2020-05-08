6e7a17c702adb4336bdc0cc66eef214efe764752
Lolligerhans <lolligerhans@gmx.de>
2020-05-08 19:49:15 +0200

    Count bishpawns twice if they have distance 1 to B
    bench 4934821

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -320,7 +320,9 @@ namespace {
                                      * (!(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles));
 
                 // Penalty for all enemy pawns x-rayed
-                score -= BishopXRayPawns * popcount(PseudoAttacks[BISHOP][s] & pos.pieces(Them, PAWN));
+                bb = PseudoAttacks[BISHOP][s] & pos.pieces(Them, PAWN);
+                bb |= shift<NORTH>(bb & PseudoAttacks[KING][s]);
+                score -= BishopXRayPawns * popcount(bb);
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
