3db756b1774278ede7e3f02f297595f290ecdabd
Lolligerhans <lolligerhans@gmx.de>
2020-06-22 05:13:35 +0200

    [4] Eg only of 2
    bench 4455947

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -163,7 +163,7 @@ namespace {
                      + WeakLever * more_than_one(lever);
 
         else if (stoppers && !opposed)
-            score += make_score(10,0);
+            score += make_score(0,10);
     }
 
     return score;
