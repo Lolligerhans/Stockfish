469f1e494062d859f6cba5f544bcc0d8a48ab92a
Lolligerhans <lolligerhans@gmx.de>
2020-06-24 07:41:43 +0200

    (2) Do not allow king to protect itself
    - compensate with higher value for king defense
    
    bench 4824541

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -473,7 +473,7 @@ namespace {
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
-                 -   4 * kingFlankDefense
+                 -   6 * kingFlankDefense
                  +  37;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
