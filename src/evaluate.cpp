e07ca3897d2bce84d3c6bc43a75a8ba31eed7cd8
Lolligerhans <lolligerhans@gmx.de>
2020-07-04 22:13:53 +0200

    Add bonus for rook attacks far down semiopen files
    bench 4901214

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -362,7 +362,12 @@ namespace {
 
             // Bonus for rook on an open or semi-open file
             if (pos.is_on_semiopen_file(Us, s))
+            {
                 score += RookOnFile[pos.is_on_semiopen_file(Them, s)];
+                score += make_score(-16, -16) + (make_score(4, 4) *
+                        (int) relative_rank(Us, frontmost_sq(Us, b))
+                        );
+            }
 
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)
