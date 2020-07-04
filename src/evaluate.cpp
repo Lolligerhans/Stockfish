debf8787f56c63428d3ab13c01accc72a2301f4d
Lolligerhans <lolligerhans@gmx.de>
2020-07-04 22:22:14 +0200

    2: Initiative 20 if queens are on board
    - Phrase as +10 if on board, -10 if not
    
    bench 4685019

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -747,6 +747,7 @@ namespace {
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
                     + 20 * (pos.count<QUEEN>() > 1)
+                    - 10
                     -110 ;
 
     Value mg = mg_value(score);
