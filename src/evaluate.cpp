44ffa7522c0c67596681e3e7f88fe8d7f3f18062
Lolligerhans <lolligerhans@gmx.de>
2020-07-23 14:45:45 +0200

    Rook on open file only if we can use it
    bench 4473303

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -369,6 +369,10 @@ namespace {
                 score += RookOnQueenFile;
 
             // Bonus for rook on an open or semi-open file
+            constexpr auto TheirHalf = Us == WHITE ? Rank5BB | Rank6BB | Rank7BB | Rank8BB
+                                                   : Rank4BB | Rank3BB | Rank2BB | Rank1BB;
+            if (b & mobilityArea[Us] & TheirHalf)
+            {
             if (pos.is_on_semiopen_file(Us, s))
                 score += RookOnFile[pos.is_on_semiopen_file(Them, s)];
 
@@ -378,6 +382,7 @@ namespace {
                 File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
                     score -= TrappedRook * (1 + !pos.castling_rights(Us));
+            }
             }
         }
 
