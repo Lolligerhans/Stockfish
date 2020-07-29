6e8be1d0b32f5105e3d239937cb91d0f037a490a
Lolligerhans <lolligerhans@gmx.de>
2020-07-29 05:45:33 +0200

    Take 3: Scale by blocked pawn count
    - Scale restricted piece bonus by pawns
    
    bench 4723604

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -553,7 +553,9 @@ namespace {
     b =   attackedBy[Them][ALL_PIECES]
        & ~stronglyProtected
        &  attackedBy[Us][ALL_PIECES];
-    score += make_score(1,1) * (pos.count<PAWN>() * popcount(b) / 2);
+    score += make_score(1,1) * ( popcount( shift<NORTH>(pos.pieces(WHITE, PAWN))
+                                         &              pos.pieces(BLACK, PAWN) )
+                               * popcount(b));
 
     // Protected or unattacked squares
     safe = ~attackedBy[Them][ALL_PIECES] | attackedBy[Us][ALL_PIECES];
