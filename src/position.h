e7634530c9edfecdb9cc1ad3e6fd8e5e75d67619
Lolligerhans <lolligerhans@gmx.de>
2020-08-01 05:23:51 +0200

    Same-color pawns not for pawns on original square
    bench 4172755

--- src/position.h
+++ src/position.h
@@ -312,7 +312,7 @@ inline bool Position::advanced_pawn_push(Move m) const {
 }
 
 inline int Position::pawns_on_same_color_squares(Color c, Square s) const {
-  return popcount(pieces(c, PAWN) & ((DarkSquares & s) ? DarkSquares : ~DarkSquares));
+  return popcount(~(Rank2BB | Rank7BB) & pieces(c, PAWN) & ((DarkSquares & s) ? DarkSquares : ~DarkSquares));
 }
 
 inline Key Position::key() const {
