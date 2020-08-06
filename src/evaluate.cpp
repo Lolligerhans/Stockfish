56065332e24e29b7eb897314b6bca09580d67eb0
Lolligerhans <lolligerhans@gmx.de>
2020-08-06 13:58:28 +0200

    Less king danger if their attack pawns are blocked
    bench 4205653

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -482,6 +482,8 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
+                 -  30 * !(pe->pawn_attacks_span(Them) & KingFlank[file_of(ksq)] & (Rank1BB | Rank8BB))
+                       * popcount(pos.pieces(Them, PAWN) & KingFlank[file_of(ksq)])
                  +  37;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
