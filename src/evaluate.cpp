dfba19ca8009f0a8f288e07141b4e6b998fa73ce
Lolligerhans <lolligerhans@gmx.de>
2020-08-10 10:39:34 +0200

    Shift up outpost ranks for reachable outposts
    bench 4310038

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -349,18 +349,18 @@ namespace {
         {
             // Bonus if the piece is on an outpost square or can reach one
             // Reduced bonus for knights (BadOutpost) if few relevant targets
-            bb = OutpostRanks & (attackedBy[Us][PAWN] | shift<Down>(pos.pieces(PAWN)))
+            bb = (attackedBy[Us][PAWN] | shift<Down>(pos.pieces(PAWN)))
                               & ~pe->pawn_attacks_span(Them);
             Bitboard targets = pos.pieces(Them) & ~pos.pieces(PAWN);
 
             if (   Pt == KNIGHT
-                && bb & s & ~CenterFiles // on a side outpost
+                && OutpostRanks & bb & s & ~CenterFiles // on a side outpost
                 && !(b & targets)        // no relevant attacks
                 && (!more_than_one(targets & (s & QueenSide ? QueenSide : KingSide))))
                 score += BadOutpost;
-            else if (bb & s)
+            else if (OutpostRanks & bb & s)
                 score += Outpost[Pt == BISHOP];
-            else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
+            else if (Pt == KNIGHT && shift<-Down>(OutpostRanks) & bb & b & ~pos.pieces(Us))
                 score += ReachableOutpost;
 
             // Bonus for a knight or bishop shielded by pawn
