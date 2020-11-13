079ab8b01b1edb9e4d3400d4c4bcf212246918c2
Lolligerhans <lolligerhans@gmx.de>
2020-11-13 22:13:17 +0100

    Offset average mobility towards S(0, 0)
    This may help reducing overestimation of Queen imbalances
    
    Bench: 3628524

--- src/psqt.cpp
+++ src/psqt.cpp
@@ -21,6 +21,8 @@
 #include "types.h"
 #include "bitboard.h"
 
+#include "offset.h"
+
 namespace PSQT {
 
 #define S(mg, eg) make_score(mg, eg)
@@ -33,44 +35,44 @@ constexpr Score Bonus[][RANK_NB][int(FILE_NB) / 2] = {
   { },
   { },
   { // Knight
-   { S(-175, -96), S(-92,-65), S(-74,-49), S(-73,-21) },
-   { S( -77, -67), S(-41,-54), S(-27,-18), S(-15,  8) },
-   { S( -61, -40), S(-17,-27), S(  6, -8), S( 12, 29) },
-   { S( -35, -35), S(  8, -2), S( 40, 13), S( 49, 28) },
-   { S( -34, -45), S( 13,-16), S( 44,  9), S( 51, 39) },
-   { S(  -9, -51), S( 22,-44), S( 58,-16), S( 53, 17) },
-   { S( -67, -69), S(-27,-50), S(  4,-51), S( 37, 12) },
-   { S(-201,-100), S(-83,-88), S(-56,-56), S(-26,-17) }
+   { S(-175, -96) - mobOffset(KNIGHT), S(-92,-65) - mobOffset(KNIGHT), S(-74,-49) - mobOffset(KNIGHT), S(-73,-21) - mobOffset(KNIGHT) },
+   { S( -77, -67) - mobOffset(KNIGHT), S(-41,-54) - mobOffset(KNIGHT), S(-27,-18) - mobOffset(KNIGHT), S(-15,  8) - mobOffset(KNIGHT) },
+   { S( -61, -40) - mobOffset(KNIGHT), S(-17,-27) - mobOffset(KNIGHT), S(  6, -8) - mobOffset(KNIGHT), S( 12, 29) - mobOffset(KNIGHT) },
+   { S( -35, -35) - mobOffset(KNIGHT), S(  8, -2) - mobOffset(KNIGHT), S( 40, 13) - mobOffset(KNIGHT), S( 49, 28) - mobOffset(KNIGHT) },
+   { S( -34, -45) - mobOffset(KNIGHT), S( 13,-16) - mobOffset(KNIGHT), S( 44,  9) - mobOffset(KNIGHT), S( 51, 39) - mobOffset(KNIGHT) },
+   { S(  -9, -51) - mobOffset(KNIGHT), S( 22,-44) - mobOffset(KNIGHT), S( 58,-16) - mobOffset(KNIGHT), S( 53, 17) - mobOffset(KNIGHT) },
+   { S( -67, -69) - mobOffset(KNIGHT), S(-27,-50) - mobOffset(KNIGHT), S(  4,-51) - mobOffset(KNIGHT), S( 37, 12) - mobOffset(KNIGHT) },
+   { S(-201,-100) - mobOffset(KNIGHT), S(-83,-88) - mobOffset(KNIGHT), S(-56,-56) - mobOffset(KNIGHT), S(-26,-17) - mobOffset(KNIGHT) }
   },
   { // Bishop
-   { S(-53,-57), S( -5,-30), S( -8,-37), S(-23,-12) },
-   { S(-15,-37), S(  8,-13), S( 19,-17), S(  4,  1) },
-   { S( -7,-16), S( 21, -1), S( -5, -2), S( 17, 10) },
-   { S( -5,-20), S( 11, -6), S( 25,  0), S( 39, 17) },
-   { S(-12,-17), S( 29, -1), S( 22,-14), S( 31, 15) },
-   { S(-16,-30), S(  6,  6), S(  1,  4), S( 11,  6) },
-   { S(-17,-31), S(-14,-20), S(  5, -1), S(  0,  1) },
-   { S(-48,-46), S(  1,-42), S(-14,-37), S(-23,-24) }
+   { S(-53,-57) - mobOffset(BISHOP), S( -5,-30) - mobOffset(BISHOP), S( -8,-37) - mobOffset(BISHOP), S(-23,-12) - mobOffset(BISHOP) },
+   { S(-15,-37) - mobOffset(BISHOP), S(  8,-13) - mobOffset(BISHOP), S( 19,-17) - mobOffset(BISHOP), S(  4,  1) - mobOffset(BISHOP) },
+   { S( -7,-16) - mobOffset(BISHOP), S( 21, -1) - mobOffset(BISHOP), S( -5, -2) - mobOffset(BISHOP), S( 17, 10) - mobOffset(BISHOP) },
+   { S( -5,-20) - mobOffset(BISHOP), S( 11, -6) - mobOffset(BISHOP), S( 25,  0) - mobOffset(BISHOP), S( 39, 17) - mobOffset(BISHOP) },
+   { S(-12,-17) - mobOffset(BISHOP), S( 29, -1) - mobOffset(BISHOP), S( 22,-14) - mobOffset(BISHOP), S( 31, 15) - mobOffset(BISHOP) },
+   { S(-16,-30) - mobOffset(BISHOP), S(  6,  6) - mobOffset(BISHOP), S(  1,  4) - mobOffset(BISHOP), S( 11,  6) - mobOffset(BISHOP) },
+   { S(-17,-31) - mobOffset(BISHOP), S(-14,-20) - mobOffset(BISHOP), S(  5, -1) - mobOffset(BISHOP), S(  0,  1) - mobOffset(BISHOP) },
+   { S(-48,-46) - mobOffset(BISHOP), S(  1,-42) - mobOffset(BISHOP), S(-14,-37) - mobOffset(BISHOP), S(-23,-24) - mobOffset(BISHOP) }
   },
   { // Rook
-   { S(-31, -9), S(-20,-13), S(-14,-10), S(-5, -9) },
-   { S(-21,-12), S(-13, -9), S( -8, -1), S( 6, -2) },
-   { S(-25,  6), S(-11, -8), S( -1, -2), S( 3, -6) },
-   { S(-13, -6), S( -5,  1), S( -4, -9), S(-6,  7) },
-   { S(-27, -5), S(-15,  8), S( -4,  7), S( 3, -6) },
-   { S(-22,  6), S( -2,  1), S(  6, -7), S(12, 10) },
-   { S( -2,  4), S( 12,  5), S( 16, 20), S(18, -5) },
-   { S(-17, 18), S(-19,  0), S( -1, 19), S( 9, 13) }
+   { S(-31, -9) - mobOffset(ROOK), S(-20,-13) - mobOffset(ROOK), S(-14,-10) - mobOffset(ROOK), S(-5, -9) - mobOffset(ROOK) },
+   { S(-21,-12) - mobOffset(ROOK), S(-13, -9) - mobOffset(ROOK), S( -8, -1) - mobOffset(ROOK), S( 6, -2) - mobOffset(ROOK) },
+   { S(-25,  6) - mobOffset(ROOK), S(-11, -8) - mobOffset(ROOK), S( -1, -2) - mobOffset(ROOK), S( 3, -6) - mobOffset(ROOK) },
+   { S(-13, -6) - mobOffset(ROOK), S( -5,  1) - mobOffset(ROOK), S( -4, -9) - mobOffset(ROOK), S(-6,  7) - mobOffset(ROOK) },
+   { S(-27, -5) - mobOffset(ROOK), S(-15,  8) - mobOffset(ROOK), S( -4,  7) - mobOffset(ROOK), S( 3, -6) - mobOffset(ROOK) },
+   { S(-22,  6) - mobOffset(ROOK), S( -2,  1) - mobOffset(ROOK), S(  6, -7) - mobOffset(ROOK), S(12, 10) - mobOffset(ROOK) },
+   { S( -2,  4) - mobOffset(ROOK), S( 12,  5) - mobOffset(ROOK), S( 16, 20) - mobOffset(ROOK), S(18, -5) - mobOffset(ROOK) },
+   { S(-17, 18) - mobOffset(ROOK), S(-19,  0) - mobOffset(ROOK), S( -1, 19) - mobOffset(ROOK), S( 9, 13) - mobOffset(ROOK) }
   },
   { // Queen
-   { S( 3,-69), S(-5,-57), S(-5,-47), S( 4,-26) },
-   { S(-3,-55), S( 5,-31), S( 8,-22), S(12, -4) },
-   { S(-3,-39), S( 6,-18), S(13, -9), S( 7,  3) },
-   { S( 4,-23), S( 5, -3), S( 9, 13), S( 8, 24) },
-   { S( 0,-29), S(14, -6), S(12,  9), S( 5, 21) },
-   { S(-4,-38), S(10,-18), S( 6,-12), S( 8,  1) },
-   { S(-5,-50), S( 6,-27), S(10,-24), S( 8, -8) },
-   { S(-2,-75), S(-2,-52), S( 1,-43), S(-2,-36) }
+   { S( 3,-69) - mobOffset(QUEEN), S(-5,-57) - mobOffset(QUEEN), S(-5,-47) - mobOffset(QUEEN), S( 4,-26) - mobOffset(QUEEN) },
+   { S(-3,-55) - mobOffset(QUEEN), S( 5,-31) - mobOffset(QUEEN), S( 8,-22) - mobOffset(QUEEN), S(12, -4) - mobOffset(QUEEN) },
+   { S(-3,-39) - mobOffset(QUEEN), S( 6,-18) - mobOffset(QUEEN), S(13, -9) - mobOffset(QUEEN), S( 7,  3) - mobOffset(QUEEN) },
+   { S( 4,-23) - mobOffset(QUEEN), S( 5, -3) - mobOffset(QUEEN), S( 9, 13) - mobOffset(QUEEN), S( 8, 24) - mobOffset(QUEEN) },
+   { S( 0,-29) - mobOffset(QUEEN), S(14, -6) - mobOffset(QUEEN), S(12,  9) - mobOffset(QUEEN), S( 5, 21) - mobOffset(QUEEN) },
+   { S(-4,-38) - mobOffset(QUEEN), S(10,-18) - mobOffset(QUEEN), S( 6,-12) - mobOffset(QUEEN), S( 8,  1) - mobOffset(QUEEN) },
+   { S(-5,-50) - mobOffset(QUEEN), S( 6,-27) - mobOffset(QUEEN), S(10,-24) - mobOffset(QUEEN), S( 8, -8) - mobOffset(QUEEN) },
+   { S(-2,-75) - mobOffset(QUEEN), S(-2,-52) - mobOffset(QUEEN), S( 1,-43) - mobOffset(QUEEN), S(-2,-36) - mobOffset(QUEEN) }
   },
   { // King
    { S(271,  1), S(327, 45), S(271, 85), S(198, 76) },
