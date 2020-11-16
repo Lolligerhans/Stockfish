4536f5fd8945e819d160f19bcc5a6cbf35f3f12e
Lolligerhans <lolligerhans@gmx.de>
2020-11-16 15:41:44 +0100

    2: Keep pushes of non-backward pawns in mobility
    Bench: 3932231

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -355,7 +355,7 @@ namespace {
 
     // Find our pawns that are blocked or on the first two ranks
     Bitboard b = pos.pieces(Us, PAWN) & (shift<Down>(pos.pieces()) | LowRanks);
-    Bitboard pwnDeny = pe->pawn_attacks(Them) & ~(shift<Up>(pe->backwards[Us]) & ~pos.pieces());
+    Bitboard pwnDeny = pe->pawn_attacks(Them) & ~(shift<Up>(pos.pieces(Us, PAWN) & ~pe->backwards[Us]) & ~pos.pieces());
 
     // Squares occupied by those pawns, by our king or queen, by blockers to attacks on our king
     // or controlled by enemy pawns are excluded from the mobility area.
