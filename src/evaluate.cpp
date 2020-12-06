1bb5e0a47a12e3a5525c4a636002936b60d59453
Lolligerhans <lolligerhans@gmx.de>
2020-12-06 05:01:40 +0100

    Allow minor protection for outposts
    Bench: 3946084

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -445,7 +445,7 @@ namespace {
         {
             // Bonus if the piece is on an outpost square or can reach one
             // Reduced bonus for knights (BadOutpost) if few relevant targets
-            bb = OutpostRanks & (attackedBy[Us][PAWN] | shift<Down>(pos.pieces(PAWN)))
+            bb = OutpostRanks & (attackedBy[Us][KNIGHT] | attackedBy[Us][BISHOP] | attackedBy[Us][PAWN] | shift<Down>(pos.pieces(PAWN)))
                               & ~pe->pawn_attacks_span(Them);
             Bitboard targets = pos.pieces(Them) & ~pos.pieces(PAWN);
 
