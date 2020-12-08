a8783f60cac3418e503bf2d41f45f5bc658a7b90
Lolligerhans <lolligerhans@gmx.de>
2020-12-08 10:27:20 +0100

    No outpost if square is attacked by N/B
    A simpler non-attacks-first version was yeelow previously. By now there
    are more outposts to apply to.
    
    Still trying to collect enough tests to maybe pass attacks-first in
    a combo.
    
    Bench: 3977285

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -446,7 +446,8 @@ namespace {
             // Bonus if the piece is on an outpost square or can reach one
             // Reduced bonus for knights (BadOutpost) if few relevant targets
             bb = OutpostRanks & (attackedBy[Us][PAWN] | shift<Down>(pos.pieces(PAWN)))
-                              & ~pe->pawn_attacks_span(Them);
+                              & ~pe->pawn_attacks_span(Them)
+                              & ~(attackedBy[Them][KNIGHT] | attackedBy[Them][BISHOP]);
             Bitboard targets = pos.pieces(Them) & ~pos.pieces(PAWN);
 
             if (   Pt == KNIGHT
