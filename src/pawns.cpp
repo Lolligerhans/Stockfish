228d01b392aab3f919dbdc78234863a82f26ca6a
Lolligerhans <lolligerhans@gmx.de>
2020-12-13 12:33:24 +0100

    Take 2: Halve impact of best and worst pawn
    Bench: 4128856

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -193,9 +193,9 @@ namespace {
     }
 
     // Double impact of best and worst pawn
-    totalScore += make_score(0, (min <  VALUE_INFINITE ? min : 0)
+    totalScore -= make_score(0, (min <  VALUE_INFINITE ? min : 0)
                               + (max > -VALUE_INFINITE ? max : 0)
-                            );
+                            ) / 2;
 
     return totalScore;
   }
