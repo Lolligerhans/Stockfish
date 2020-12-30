5da18d5054d8b50ce5c05a502a8bbd03e7c8f2b4
Lolligerhans <lolligerhans@gmx.de>
2020-12-31 00:09:35 +0100

    Increase king danger if blocked by our pieces
    Bench: 4165106

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -593,7 +593,11 @@ namespace {
     int kingFlankAttack  = popcount(b1) + popcount(b2);
     int kingFlankDefense = popcount(b3);
 
+    int selfBlock        = popcount(pos.pieces(Us) & attackedBy[Us][KING]);
+    selfBlock *= selfBlock;
+
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them] // (~10 Elo)
+                 +  10 * selfBlock
                  + 183 * popcount(kingRing[Us] & weak)                        // (~15 Elo)
                  + 148 * popcount(unsafeChecks)                               // (~4 Elo)
                  +  98 * popcount(pos.blockers_for_king(Us))                  // (~2 Elo)
