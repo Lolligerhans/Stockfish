07156ecbb8fbc5c7dcf4984c6766dfb07e89d803
Lolligerhans <lolligerhans@gmx.de>
2021-01-05 01:54:26 +0100

    Take 3: [-3, 5] +100%
    - Tweak edge cases of outflanking
    
    Bench: 4036776

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -882,7 +882,7 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 12 * pos.count<PAWN>()
-                    + 13 * outflanking
+                    + 18 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
