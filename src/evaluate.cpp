3a275cffc47440f37cda153105b42fbd81ef6a9b
Lolligerhans <lolligerhans@gmx.de>
2021-01-05 18:28:08 +0100

    Take 2: Scale p_both_flanks with outflanking
    - Extra initiative bonus if pawns_on_both_flanks and outflanking bonus
    synergize well
    
    Bench: 4367382

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -882,7 +882,7 @@ namespace {
     int complexity =   9 * pe->passed_count()
                     + 12 * pos.count<PAWN>()
                     +  9 * outflanking
-                    + (outflanking >= 2 ? 45 : 21) * pawnsOnBothFlanks
+                    + (21 + 10 * std::max(0, outflanking)) * pawnsOnBothFlanks
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
