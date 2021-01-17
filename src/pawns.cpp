aade3f1fd40cafa2f4198b0877a585747cc9245c
Lolligerhans <lolligerhans@gmx.de>
2021-01-17 09:56:24 +0100

    Reduced support/phalanx bonus in early-game
    Bench: 4248254

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -161,7 +161,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
+            int v =  Connected[r] * (2 - early + bool(phalanx) - bool(opposed))
                    + 22 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
