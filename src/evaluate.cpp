d710f0bf6206fefddaf6cde36a9e1fa47eee6b0d
Lolligerhans <lolligerhans@gmx.de>
2021-01-23 21:17:22 +0100

    Reverse effect
    - adjust Pawns-on-same-color penalty by pawns on other color
    
    Bench: 3883692

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,13 +457,10 @@ namespace {
                 // when the bishop is outside the pawn chain.
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
-                const auto n = std::max(
-                        0,
-                        // dont allow negatives here
-                        2 * pos.pawns_on_same_color_squares(Us, s) - pos.count<PAWN>(Us)
-                        );
+                auto n = pos.pawns_on_same_color_squares(Us, s) + pos.count<PAWN>(Us);
                 score -= BishopPawns[edge_distance(file_of(s))] * n
-                                     * (!(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles));
+                                     * (!(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles))
+                                     / 3;
 
                 // Penalty for all enemy pawns x-rayed
                 score -= BishopXRayPawns * popcount(attacks_bb<BISHOP>(s) & pos.pieces(Them, PAWN));
