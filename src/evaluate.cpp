be6e47f7879c5a2b52074e9055408d04bc0e68ba
Lolligerhans <lolligerhans@gmx.de>
2021-01-23 21:13:37 +0100

    Continue npm-initiative into non-pawnendgame
    Bench: 4327218

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -884,7 +884,7 @@ namespace {
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
-                    + 51 * !pos.non_pawn_material()
+                    + 51 * (pos.non_pawn_material(WHITE) == pos.non_pawn_material(BLACK))
                     - 43 * almostUnwinnable
                     -110 ;
 
