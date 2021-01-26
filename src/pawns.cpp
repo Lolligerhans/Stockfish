d9c0bd7652b6d23826d343012863fe07d3bc3286
Lolligerhans <lolligerhans@gmx.de>
2021-01-26 21:19:49 +0100

    No phalanx bonus if last pawn
    Bench: 4228277

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -160,7 +160,7 @@ namespace {
             e->passedPawns[Us] |= s;
 
         // Score this pawn
-        if (support | phalanx)
+        if (support || (phalanx && neighbours & forward_ranks_bb(Them, s)))
         {
             int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
                    + 22 * popcount(support);
