ed8b7dea3a01e0728408126a26c659bcd47e5542
Lolligerhans <lolligerhans@gmx.de>
2021-01-31 17:44:01 +0100

    Support bonus for passed pawns
    | I imagine has been tried before. (?) Maybe not with the !blocked
    addition.
    
    Bench: 4287672

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -162,7 +162,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
+            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed) + (passed && !blocked))
                    + 22 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
