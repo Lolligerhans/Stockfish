f173a438ae709b3c1d85a50673359f729d3df2eb
Lolligerhans <lolligerhans@gmx.de>
2021-05-06 17:02:32 +0200

    Take 2: Not doubled if phalanx but extra penalty
    Bench: 4126987

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -165,7 +165,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
+            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed) - bool(doubled))
                    + 22 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
