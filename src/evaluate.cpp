cf478018f5420f3ee5bf174a5db90b0998d802a3
Lolligerhans <lolligerhans@gmx.de>
2021-05-13 11:39:23 +0200

    More complexity for higher material endgames
    Bench: 3759061

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -907,7 +907,7 @@ namespace {
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
-                    + 51 * !pos.non_pawn_material()
+                    + 51 * (pos.non_pawn_material() >= 2*QueenValueMg || !pos.non_pawn_material())
                     - 43 * almostUnwinnable
                     -110 ;
 
@@ -917,7 +917,7 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((mg > 0) - (mg < 0)) * std::clamp(complexity + 50, -abs(mg), 0);
+    int u = ((mg > 0) - (mg < 0)) * std::clamp(complexity, -abs(mg), 0);
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
     mg += u;
