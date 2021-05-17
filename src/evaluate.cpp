9667e18d4d7c1fbbf251e2c34dd09b082fa00bad
Lolligerhans <lolligerhans@gmx.de>
2021-05-17 19:09:43 +0200

    Take 3: max 1/4
     - Max MG complexity reduction instead of +50
    
    Bench: 4414907

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -921,7 +921,7 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((mg > 0) - (mg < 0)) * std::clamp(complexity, -abs(mg)/2, 0);
+    int u = ((mg > 0) - (mg < 0)) * std::clamp(complexity, -abs(mg)/4, 0);
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
     mg += u;
