21629b385536123272445a4b84d87df8e90cb320
Lolligerhans <lolligerhans@gmx.de>
2021-06-21 13:43:06 +0200

    Connected bonus for stoppers on adjacent files
    Bench: 4848650

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -165,7 +165,8 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
+            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed)
+                                       + bool(stoppers ^ opposed))
                    + 22 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
