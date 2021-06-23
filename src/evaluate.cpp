54247c19eef0275b0b67b725a9876e16db937e4c
Lolligerhans <lolligerhans@gmx.de>
2021-06-23 04:20:51 +0200

    Take 2: Remove
    - Soften shuffle damping
    
    Bench: 4513151

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1112,9 +1112,6 @@ Value Eval::evaluate(const Position& pos) {
                     : adjusted_NNUE();                   // NNUE
   }
 
-  // Damp down the evaluation linearly when shuffling
-  v = v * (128 - pos.rule50_count()) / 128;
-
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
