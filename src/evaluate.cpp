4f1d52e6262ee01d1ec21dc2cea355bc8555e13a
Lolligerhans <lolligerhans@gmx.de>
2021-06-24 19:50:08 +0200

    More uncontested bonus if attacking pawns
    Bench: 4870125

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -439,7 +439,8 @@ namespace {
                 && bb & s & ~CenterFiles // on a side outpost
                 && !(b & targets)        // no relevant attacks
                 && (!more_than_one(targets & (s & QueenSide ? QueenSide : KingSide))))
-                score += UncontestedOutpost * popcount(pos.pieces(PAWN) & (s & QueenSide ? QueenSide : KingSide));
+                score += UncontestedOutpost * popcount(pos.pieces(PAWN) & (s & QueenSide ? QueenSide : KingSide))
+                                            * (1 + bool(pos.pieces(Them, PAWN) & ~attackedBy[Them][PAWN]));
             else if (bb & s)
                 score += Outpost[Pt == BISHOP];
             else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
